9fad89725dacbe374119278d6b69eb1587f79b75
ScobarDen <gdv-ps@yandex.ru>
2022-12-01 21:02:09 +0300

    add User registration

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
 const {Router} = require('express');
+const bcrypt = require('bcrypt');
 const User = require('../models/User')
 const router = Router();
 
@@ -6,6 +7,17 @@ const router = Router();
 router.post('/register', async (req, res) => {
     try {
         const {email, password} = req.body;
+        const candidate = await User.findOne({email});
+
+        if (candidate) {
+           return  res.status(400).json({message: 'Такой пользователь уже существует'});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const user = new User({email, password: hashedPassword});
+        await user.save();
+        res.status(201).json({message: 'Пользователь создан'})
+
     } catch (err) {
         res.status(500).json({message: 'Что-то пошло не так'});
     }
