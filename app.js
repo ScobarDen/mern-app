eff2fb5f5a82fbeb7a55d62f857512bbaaea148b
DenScobar <gdv201099@gmail.com>
2022-12-01 16:20:43 +0300

    add mongo

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = config.get('port') || 3000;
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'),{
+
+        });
+    } catch (err) {
+        console.log('Server error: ' + err.toString());
+        process.exit(1);
+    }
+}
+
+start();
+
 
 app.listen(PORT, () => console.log(`App has been started on port ${PORT} ...`));
