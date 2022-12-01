8cf7543565b94e416f3202ad565b96da7c6ab32c
ScobarDen <gdv-ps@yandex.ru>
2022-12-01 20:50:58 +0300

    add User model

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,18 @@
 const {Router} = require('express');
+const User = require('../models/User')
 const router = Router();
 
 // /api/auth/register
 router.post('/register', async (req, res) => {
+    try {
+        const {email, password} = req.body;
+    } catch (err) {
+        res.status(500).json({message: 'Что-то пошло не так'});
+    }
+});
+
+// /api/auth/login
+router.post('/login', async (req, res) => {
 
 });
 
