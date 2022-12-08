844448408325dc2b79a17257811cbab9b3cc6188
ScobarDen <gdv-ps@mail.ru>
2022-12-08 14:40:50 +0300

    finish creating useHttp

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -4,8 +4,26 @@ export const useHttp = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const request = useCallback(async (url, method = "GET", body = null, headers = {}) => {
+        setLoading(true);
+        try {
+            const response = await fetch(url,{method, body, headers});
+            const data = await response.json();
 
-    });
+            if (!response.ok) {
+                throw new Error(data.message || "Что-то пошло не так");
+            }
 
-    return {loading, request, error};
+            setLoading(false);
+            return data;
+
+        } catch (e) {
+            setLoading(false);
+            setError(e.message);
+            throw e;
+        }
+    }, []);
+
+    const clearError = () => setError(null);
+
+    return {loading, request, error, clearError};
 }
