21d41c0a6b206ab762b46e7057f94767eddbf95c
ScobarDen <gdv-ps@yandex.ru>
2022-12-08 20:43:19 +0300

    finish authentification

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,8 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {useHttp} from "../hooks/http.hook";
 import {useMessage} from "../hooks/message.hook";
+import {AuthContext} from "../context/AuthContext";
 
 const AuthPage = () => {
+    const auth = useContext(AuthContext);
     const {loading, request, error, clearError} = useHttp();
     const [form, setForm] = useState({
         email: "", password: ""
@@ -30,7 +32,7 @@ const AuthPage = () => {
     const loginHandler = async () => {
         try {
             const data = await request("/api/auth/login", "POST", {...form});
-            message(data.message);
+            auth.login(data.token, data.userId);
         } catch (err) {
         }
     }
